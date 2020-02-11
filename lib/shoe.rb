188bd7f60c4ff4f9ba6e1688b0db9ab07f37b9e7
Samantha Meason <sbmeason@gmail.com>
2020-02-11 00:06:39 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -8,12 +8,10 @@ class Shoe
     @brand = brand
     if !(BRANDS.include?(@brand))
       BRANDS << @brand
-  end
+    end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
-
 end
